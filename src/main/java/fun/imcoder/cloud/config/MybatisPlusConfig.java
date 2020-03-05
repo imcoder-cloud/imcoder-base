d98b5f8d617af44e0f9f59ff479e0f011b18a293
dongdongchen <dd18355193335@163.com>
2020-03-05 15:29:17 +0800

    优化结构

--- src/main/java/fun/imcoder/cloud/config/MybatisPlusConfig.java
+++ src/main/java/fun/imcoder/cloud/config/MybatisPlusConfig.java
@@ -1,7 +1,6 @@
 package fun.imcoder.cloud.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
