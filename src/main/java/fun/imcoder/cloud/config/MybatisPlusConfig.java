3a01611035a75bba6077d77ae292cd430abd5599
dongdongchen <dd18355193335@163.com>
2020-03-18 17:08:21 +0800

    修改mybatis-plus请求页大于最后页返回首页

--- src/main/java/fun/imcoder/cloud/config/MybatisPlusConfig.java
+++ src/main/java/fun/imcoder/cloud/config/MybatisPlusConfig.java
@@ -13,7 +13,7 @@ public class MybatisPlusConfig {
     public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
         // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
-        // paginationInterceptor.setOverflow(false);
+        paginationInterceptor.setOverflow(true);
         // 设置最大单页限制数量，默认 500 条，-1 不受限制
         // paginationInterceptor.setLimit(500);
         return paginationInterceptor;
